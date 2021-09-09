f974a77aee6ddd613667fadc10795b98593f1c8b
Salomão Junio Santos Batista <salomaojunio@hotmail.com>
2021-09-09 15:14:06 -0300

    Previous Updates
    Previous updates that were pending

--- app/src/main/java/br/com/salosoftwares/mobile/imc_application/MainActivity.kt
+++ app/src/main/java/br/com/salosoftwares/mobile/imc_application/MainActivity.kt
@@ -2,10 +2,41 @@ package br.com.salosoftwares.mobile.imc_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //finish() = Encerra a aplicação
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.w("LifeCycle","START- Iniciado, a tela está visível")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("LifeCycle","RESUME- Interagindo rsrs")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("LifeCycle","PAUSE- A tela perdeu o foco, não é possível interagir")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("LifeCycle","STOP- A tela não está mais visível, mas ainda existe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("LifeCycle","RESTART- Retomando o Foco !")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("LifeCycle","GAME OVER")
     }
 }
---
+++ app/src/main/java/br/com/salosoftwares/mobile/imc_application/ResultActivity.kt
@@ -0,0 +1,14 @@
+package br.com.salosoftwares.mobile.imc_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+
+
+}
